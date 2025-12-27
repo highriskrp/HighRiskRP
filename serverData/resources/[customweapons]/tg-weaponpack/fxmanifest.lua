916726f8f22caac7a49409d361cd582806b99414
DaniDiamondzttv <tnggops@gmail.com>
2025-12-27 09:04:44 -0600

    1

---
+++ serverData/resources/[customweapons]/tg-weaponpack/fxmanifest.lua
@@ -0,0 +1,254 @@
+fx_version 'adamant'
+game 'gta5'
+author 'TrickGodness'
+description 'Weapon Pack'
+
+client_script 'cl_weaponNames.lua'
+client_script 'names.lua'
+
+files {
+    'data/pocketlight/dlctext.meta',
+    'data/pocketlight/pickups.meta',
+    'data/pocketlight/contentunlocks.meta',
+    'data/pocketlight/shop_weapon.meta',
+    'data/pocketlight/pedpersonality.meta',
+    'data/pocketlight/weaponarchetypes.meta',
+    'data/pocketlight/weaponanimations.meta',
+	'data/pocketlight/weaponcomponents.meta',
+	'data/pocketlight/weapon_pocketlight.meta',
+}
+
+data_file 'TEXTFILE_METAFILE' 'data/pocketlight/dlctext.meta'
+data_file 'DLC_WEAPON_PICKUPS' 'data/pocketlight/pickups.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/pocketlight/contentunlocks.meta'
+data_file 'WEAPON_SHOP_INFO_METADATA_FILE' 'data/pocketlight/shop_weapon.meta'
+data_file 'PED_PERSONALITY_FILE' 'data/pocketlight/pedpersonality.meta'
+data_file 'WEAPON_METADATA_FILE' 'data/pocketlight/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'data/pocketlight/weaponanimations.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'data/pocketlight/weaponcomponents.meta'
+data_file 'WEAPONINFO_FILE' 'data/pocketlight/weapon_pocketlight.meta'
+
+----------------------------------------------------------------------------------------
+
+files{
+	'data/m4a1/components/COMPONENT_M4A1FM_BARREL/weaponcomponents.meta',
+    'data/m4a1/components/COMPONENT_M4A1FM_CLIP/weaponcomponents.meta',
+    'data/m4a1/components/COMPONENT_M4A1FM_FLSH/weaponcomponents.meta',
+    'data/m4a1/components/COMPONENT_M4A1FM_SCOPE/weaponcomponents.meta',
+	'data/m4a1/weaponarchetypes.meta',
+	'data/m4a1/weaponanimations.meta',
+	'data/m4a1/pedpersonality.meta',
+	'data/m4a1/weapons.meta',
+}
+
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'data/m4a1/components/COMPONENT_M4A1FM_BARREL/weaponcomponents.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'data/m4a1/components/COMPONENT_M4A1FM_CLIP/weaponcomponents.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'data/m4a1/components/COMPONENT_M4A1FM_FLSH/weaponcomponents.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'data/m4a1/components/COMPONENT_M4A1FM_SCOPE/weaponcomponents.meta'
+data_file 'WEAPON_METADATA_FILE' 'data/m4a1/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'data/m4a1/weaponanimations.meta'
+data_file 'PED_PERSONALITY_FILE' 'data/m4a1/pedpersonality.meta'
+data_file 'WEAPONINFO_FILE' 'data/m4a1/weapons.meta'
+
+----------------------------------------------------------------------------------------
+
+files {
+    'data/hk416/dlctext.meta',
+    'data/hk416/pickups.meta',
+    'data/hk416/shop_weapon.meta',
+    'data/hk416/pedpersonality.meta',
+    'data/hk416/weaponarchetypes.meta',
+    'data/hk416/weaponanimations.meta',
+	'data/hk416/weaponcomponents.meta',
+	'data/hk416/HK416Gunlocks.meta',
+    'data/hk416/weapons.meta',
+}
+
+data_file 'TEXTFILE_METAFILE' 'data/hk416/dlctext.meta'
+data_file 'DLC_WEAPON_PICKUPS' 'data/hk416/pickups.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/hk416/HK416Gunlocks.meta'
+data_file 'WEAPON_SHOP_INFO_METADATA_FILE' 'data/hk416/shop_weapon.meta'
+data_file 'PED_PERSONALITY_FILE' 'data/hk416/pedpersonality.meta'
+data_file 'WEAPON_METADATA_FILE' 'data/hk416/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'data/hk416/weaponanimations.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'data/hk416/weaponcomponents.meta'
+data_file 'WEAPONINFO_FILE' 'data/hk416/weapons.meta'
+
+----------------------------------------------------------------------------------------
+
+files{
+	'data/tacticalshotgun/weaponarchetypes.meta',
+	'data/tacticalshotgun/weaponanimations.meta',
+	'data/tacticalshotgun/pedpersonality.meta',
+	'data/tacticalshotgun/weapons.meta',
+}
+
+data_file 'WEAPON_METADATA_FILE' 'data/tacticalshotgun/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'data/tacticalshotgun/weaponanimations.meta'
+data_file 'PED_PERSONALITY_FILE' 'data/tacticalshotgun/pedpersonality.meta'
+data_file 'WEAPONINFO_FILE' 'data/tacticalshotgun/weapons.meta'
+
+----------------------------------------------------------------------------------------
+
+files {
+    'data/heavymk2/dlctext.meta',
+    'data/heavymk2/shop_weapon.meta',
+    'data/heavymk2/pedpersonality.meta',
+    'data/heavymk2/weaponarchetypes.meta',
+    'data/heavymk2/weaponanimations.meta',
+	'data/heavymk2/weaponcomponents.meta',
+	'data/heavymk2/contentunlocks.meta',
+    'data/heavymk2/weapons_heavyshotgun_mk2.meta',
+    'audio/heavyshotgunmk2_game.dat151.rel',
+    'audio/heavyshotgunmk2_sounds.dat54.rel',
+}
+
+data_file 'TEXTFILE_METAFILE' 'data/heavymk2/dlctext.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/heavymk2/contentunlocks.meta'
+data_file 'WEAPON_SHOP_INFO_METADATA_FILE' 'data/heavymk2/shop_weapon.meta'
+data_file 'PED_PERSONALITY_FILE' 'data/heavymk2/pedpersonality.meta'
+data_file 'WEAPON_METADATA_FILE' 'data/heavymk2/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'data/heavymk2/weaponanimations.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'data/heavymk2/weaponcomponents.meta'
+data_file 'WEAPONINFO_FILE' 'data/heavymk2/weapons_heavyshotgun_mk2.meta'
+data_file 'AUDIO_GAMEDATA' 'audio/heavyshotgunmk2_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/heavyshotgunmk2_sounds.dat'
+
+----------------------------------------------------------------------------------------
+
+files{
+	'data/nlaw/weaponarchetypes.meta',
+	'data/nlaw/weaponanimations.meta',
+	'data/nlaw/pedpersonality.meta',
+	'data/nlaw/weapons.meta',
+}
+
+data_file 'WEAPON_METADATA_FILE' 'data/nlaw/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'data/nlaw/weaponanimations.meta'
+data_file 'PED_PERSONALITY_FILE' 'data/nlaw/pedpersonality.meta'
+data_file 'WEAPONINFO_FILE' 'data/nlaw/weapons.meta'
+
+----------------------------------------------------------------------------------------
+
+files{
+	'data/candyaxe/weaponarchetypes.meta',
+	'data/candyaxe/weaponanimations.meta',
+	'data/candyaxe/pedpersonality.meta',
+	'data/candyaxe/weapons.meta',
+}
+
+data_file 'WEAPON_METADATA_FILE' 'data/candyaxe/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'data/candyaxe/weaponanimations.meta'
+data_file 'PED_PERSONALITY_FILE' 'data/candyaxe/pedpersonality.meta'
+data_file 'WEAPONINFO_FILE' 'data/candyaxe/weapons.meta'
+
+----------------------------------------------------------------------------------------
+
+files {
+    'data/amrifle/dlctext.meta',
+    'data/amrifle/shop_weapon.meta',
+    'data/amrifle/pedpersonality.meta',
+    'data/amrifle/weaponarchetypes.meta',
+    'data/amrifle/weaponanimations.meta',
+	'data/amrifle/weaponcomponents.meta',
+	'data/amrifle/amriflecontentunlocks.meta',
+    'data/amrifle/pickups.meta',
+    'data/amrifle/weapons.meta',
+    'audio/amrifle_sounds.dat54.rel',
+    'audio/amrifle_game.dat151.rel',
+}
+
+data_file 'TEXTFILE_METAFILE' 'data/amrifle/dlctext.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/amrifle/amriflecontentunlocks.meta'
+data_file 'WEAPON_SHOP_INFO_METADATA_FILE' 'data/amrifle/shop_weapon.meta'
+data_file 'PED_PERSONALITY_FILE' 'data/amrifle/pedpersonality.meta'
+data_file 'WEAPON_METADATA_FILE' 'data/amrifle/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'data/amrifle/weaponanimations.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'data/amrifle/weaponcomponents.meta'
+data_file 'WEAPONINFO_FILE' 'data/amrifle/weapons.meta'
+data_file 'DLC_WEAPON_PICKUPS' 'data/amrifle/pickups.meta'
+data_file 'AUDIO_GAMEDATA' 'audio/amrifle_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/amrifle_sounds.dat'
+
+----------------------------------------------------------------------------------------
+
+files{
+	'data/vp9/components/COMPONENT_AT_VP9_FLSH/weaponcomponents.meta',
+    'data/vp9/components/COMPONENT_VP9_CLIP_01/weaponcomponents.meta',
+	'data/vp9/weaponarchetypes.meta',
+	'data/vp9/weaponanimations.meta',
+	'data/vp9/pedpersonality.meta',
+	'data/vp9/weapons.meta',
+}
+
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'data/vp9/components/COMPONENT_AT_VP9_FLSH/weaponcomponents.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'data/vp9/components/COMPONENT_VP9_CLIP_01/weaponcomponents.meta'
+data_file 'WEAPON_METADATA_FILE' 'data/vp9/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'data/vp9/weaponanimations.meta'
+data_file 'PED_PERSONALITY_FILE' 'data/vp9/pedpersonality.meta'
+data_file 'WEAPONINFO_FILE' 'data/vp9/weapons.meta'
+
+----------------------------------------------------------------------------------------
+
+files {
+    'data/minishotgun/dlctext.meta',
+    'data/minishotgun/shop_weapon.meta',
+    'data/minishotgun/pedpersonality.meta',
+    'data/minishotgun/weaponarchetypes.meta',
+    'data/minishotgun/weaponanimations.meta',
+	'data/minishotgun/weaponcomponents.meta',
+	'data/minishotgun/minishotguncontentunlocks.meta',
+    'data/minishotgun/pickups.meta',
+    'data/minishotgun/weapons.meta',
+}
+
+data_file 'TEXTFILE_METAFILE' 'data/minishotgun/dlctext.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/minishotgun/minishotguncontentunlocks.meta'
+data_file 'WEAPON_SHOP_INFO_METADATA_FILE' 'data/minishotgun/shop_weapon.meta'
+data_file 'PED_PERSONALITY_FILE' 'data/minishotgun/pedpersonality.meta'
+data_file 'WEAPON_METADATA_FILE' 'data/minishotgun/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'data/minishotgun/weaponanimations.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'data/minishotgun/weaponcomponents.meta'
+data_file 'WEAPONINFO_FILE' 'data/minishotgun/weapons.meta'
+data_file 'DLC_WEAPON_PICKUPS' 'data/minishotgun/pickups.meta'
+
+
+----------------------------------------------------------------------------------------
+
+files{
+	'data/glock19gen4/weaponarchetypes.meta',
+	'data/glock19gen4/weaponanimations.meta',
+    'data/glock19gen4/weaponcomponents.meta',
+	'data/glock19gen4/pedpersonality.meta',
+	'data/glock19gen4/weapons.meta',
+}
+
+data_file 'WEAPON_METADATA_FILE' 'data/glock19gen4/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'data/glock19gen4/weaponanimations.meta'
+data_file 'PED_PERSONALITY_FILE' 'data/glock19gen4/pedpersonality.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'data/glock19gen4/weaponcomponents.meta'
+data_file 'WEAPONINFO_FILE' 'data/glock19gen4/weapons.meta'
+
+----------------------------------------------------------------------------------------
+
+files {
+    'data/vityaz/dlctext.meta',
+    'data/vityaz/shop_weapon.meta',
+    'data/vityaz/pedpersonality.meta',
+    'data/vityaz/weaponarchetypes.meta',
+    'data/vityaz/weaponanimations.meta',
+	'data/vityaz/weaponcomponents.meta',
+	'data/vityaz/vityazcontentunlocks.meta',
+    'data/vityaz/pickups.meta',
+    'data/vityaz/weaponvityaz.meta',
+}
+
+data_file 'TEXTFILE_METAFILE' 'data/vityaz/dlctext.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/vityaz/vityazcontentunlocks.meta'
+data_file 'WEAPON_SHOP_INFO_METADATA_FILE' 'data/vityaz/shop_weapon.meta'
+data_file 'PED_PERSONALITY_FILE' 'data/vityaz/pedpersonality.meta'
+data_file 'WEAPON_METADATA_FILE' 'data/vityaz/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'data/vityaz/weaponanimations.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'data/vityaz/weaponcomponents.meta'
+data_file 'WEAPONINFO_FILE' 'data/vityaz/weaponvityaz.meta'
+data_file 'DLC_WEAPON_PICKUPS' 'data/vityaz/pickups.meta'
