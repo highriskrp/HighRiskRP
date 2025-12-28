87fe369d04ca849620437b48efa164aa0d778d3d
JustNikki58 <rc4lyfe58@gmail.com>
2025-12-27 21:17:04 -0600

    veh sounds 1210291029

--- serverData/resources/Vehicle_Sounds/fxmanifest.lua
+++ serverData/resources/Vehicle_Sounds/fxmanifest.lua
@@ -6,9 +6,16 @@ files {
   'audioconfig/aq93bmws55_game.dat151.rel',
   'audioconfig/aq93bmws55_sounds.dat54.rel',
   'sfx/dlc_aq93bmws55/aq93bmws55.awc',
-  'sfx/dlc_aq93bmws55/aq93bmws55_npc.awc'
+  'sfx/dlc_aq93bmws55/aq93bmws55_npc.awc',
+  'audioconfig/npolchar_game.dat151.rel',
+  'audioconfig/npolchar_sounds.dat54.rel',
+  'sfx/dlc_npolchar/npolchar.awc',
+  'sfx/dlc_npolchar/npolchar_npc.awc'
 }
 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/aq93bmws55_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/aq93bmws55_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aq93bmws55'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/npolchar_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/npolchar_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_npolchar'
