dc6e4ae20c797a61451088d209f67919ef2d1550
JustNikki58 <rc4lyfe58@gmail.com>
2025-12-27 21:05:50 -0600

    veh sounds round 29232380

---
+++ serverData/resources/Vehicle_Sounds/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean' -- change to whatever fx version is used
+
+game 'gta5'
+
+files {
+  'audioconfig/aq93bmws55_game.dat151.rel',
+  'audioconfig/aq93bmws55_sounds.dat54.rel',
+  'sfx/dlc_aq93bmws55/aq93bmws55.awc',
+  'sfx/dlc_aq93bmws55/aq93bmws55_npc.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/aq93bmws55_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/aq93bmws55_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aq93bmws55'
